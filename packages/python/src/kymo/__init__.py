4f52230a93958b7a5db0e6b0fe67a4024433f3c2
Vu Anh <anhv.ict91@gmail.com>
2026-05-22 06:47:58 +0700

    chore(release): bump kymostudio to 0.2.5

--- packages/python/src/kymo/__init__.py
+++ packages/python/src/kymo/__init__.py
@@ -19,7 +19,7 @@ from .layout import layout
 from .model import Component, Diagram, Edge, Region
 from .to_svg import render
 
-__version__ = "0.2.4"
+__version__ = "0.2.5"
 
 __all__ = [
     "parse",
