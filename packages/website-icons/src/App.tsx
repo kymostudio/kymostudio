4b47ad5a16f81678ba8c5a28c9f1f86d27327226
Vu Anh <anhv.ict91@gmail.com>
2026-06-21 10:55:57 +0700

    chore(icons): brand page route /brand/<slug>

--- packages/website-icons/src/App.tsx
+++ packages/website-icons/src/App.tsx
@@ -316,7 +316,7 @@ export function App() {
             <div className="dlg-body">
               <div className="dlg-head">
                 {dialog.variants
-                  ? <a className="dlg-key dlg-link" href={`/brand/${dialog.set}/${dialog.key.split(":")[1]}`}>{dialog.name || av.key} ↗</a>
+                  ? <a className="dlg-key dlg-link" href={`/brand/${dialog.key.split(":")[1]}`}>{dialog.name || av.key} ↗</a>
                   : <span className="dlg-key">{dialog.name || av.key}</span>}
                 <span className="dlg-set">{dialog.set}</span>
               </div>
